3cde22ce957cafb4905e8ed24f142c6b5e62fdb9
Romain VORLET <romain.vorlet@gmail.com>
2023-11-01 14:52:03 +0100

    deux cibles

--- object_generator.py
+++ object_generator.py
@@ -12,12 +12,13 @@ running = True
 dt = 0
 screen.fill("grey")
 ORANGE = (255, 127, 0)
+NOIR = (0,0,0)
 
 pos_x_object = 0
 pos_y_object = 0
 WIDTH_OBJECT = 70
 HEIGHT_OBJECT = 50
-FPS = 10
+FPS = 30
 clock = pygame.time.Clock()
 number_of_enemies = 10
 number_of_target = 5
@@ -41,10 +42,27 @@ while len(object_list) < number_of_enemies:
         object_list.append(object_creation_process)
 
 while len(other_object) < number_of_target:
-    object_creation_process = pygame.Rect(random.randint(0, largeur_ecran - WIDTH_OBJECT), 0, WIDTH_OBJECT,
-                                          HEIGHT_OBJECT)
+    object_creation_process = pygame.Rect(random.randint(0, largeur_ecran - WIDTH_OBJECT), 0, WIDTH_OBJECT//2,
+                                          HEIGHT_OBJECT//2)
+    not_colliding_with_existing_object = True
 
-assert len(object_list) == number_of_enemies, "La liste des rectangles est > 10"
+    for obj in object_list:
+        if obj.colliderect(object_creation_process):
+            not_colliding_with_existing_object = False
+
+    not_colliding = True
+
+    if not_colliding_with_existing_object:
+        for other in other_object:
+            if other.colliderect(object_creation_process):
+                not_colliding = False
+
+    if not_colliding and not_colliding_with_existing_object:
+        other_object.append(object_creation_process)
+
+
+assert len(object_list) == number_of_enemies, "La liste des rectangles est différente de 10"
+assert len(other_object) == number_of_target, "La liste des cibles est différente de 5"
 
 while running:
     for event in pygame.event.get():
@@ -55,13 +73,17 @@ while running:
                 if obj.collidepoint(event.pos):
                     object_list.remove(obj)
                     score += 200
+            for other in other_object:
+                if other.collidepoint(event.pos):
+                    other_object.remove(other)
+                    score += 200
 
     screen.fill("grey")
 
     for obj in object_list:
         pygame.draw.rect(screen, ORANGE, obj, 1)
-        obj.y += 10
-        obj.x += 4
+        obj.y += (15/FPS)
+        obj.x += (9/FPS)
         if obj.y > hauteur_ecran:
             object_list.remove(obj)
         if obj.x > largeur_ecran:
@@ -69,6 +91,11 @@ while running:
             score -= 50
     if len(object_list) == 0:
         pygame.quit()
+
+    for other in other_object:
+        pygame.draw.rect(screen, NOIR, other,1)
+        other.y += (15/FPS)
+        other.x += (9/FPS)
 
     pos_y_object = pos_y_object + 10
     print("pos_y : ", pos_y_object)
